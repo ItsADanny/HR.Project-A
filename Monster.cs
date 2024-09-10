2efe283edfe833d14da7a8425f5307fb020845fa
ItsDanny <danny.desnoo@ddesnoo.nl>
2024-09-10 15:27:14 +0200

    Added the Monster en Quest class files

---
+++ Monster.cs
@@ -0,0 +1,42 @@
+public class Monster
+{
+    public int ID;
+    public string Name;
+    public string Description;
+    public int Health;
+    public int DamageRangeMin;
+    public int DamageRangeMax;
+    
+    public Monster(int id, string name, string description, int health, int damage_range_min, int damage_range_max)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+        Health = health;
+        DamageRangeMin = damage_range_min;
+        DamageRangeMax = damage_range_max;
+    }
+
+    public int GenAttackDamage() {
+        Random rnd = new Random();
+        return rnd.Next(DamageRangeMin, (DamageRangeMax + 1));
+    }
+
+    public void TakeDamage(int damage) {
+        if (Health - damage < 0) {
+            Health = 0;
+        } else {
+            Health -= damage;
+        }
+    }
+
+    public void isAlive() {
+        if (Health > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    
+
+}
